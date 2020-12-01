3cda920e964a2a798ca8a56b8eb44d5e1ebd669b
Peter Marsh <breadloaf64@gmail.com>
2020-12-01 23:55:18 +0000

    Refactored into wrapper intlist class.

---
+++ src/Helpers/IntList.java
@@ -0,0 +1,64 @@
+package Helpers;
+
+import java.util.ArrayList;
+
+public class IntList extends ArrayList<Integer>{
+    public IntList() {
+        super();
+    }
+
+    public IntList(ArrayList<String> strings) {
+        super();
+        for(String s : strings) {
+            this.add(Integer.parseInt(s));
+        }
+    }
+
+    public void print() {
+        for(Integer i : this) {
+            System.out.println(i);
+        }
+    }
+
+    public long sum() {
+        long sum = 0;
+        for(Integer i : this) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public long sum(MultiDimIndex is) {
+        int sum = 0;
+        for(int i = 0; i < is.length(); i++) {
+            sum += this.get(is.getIndex(i));
+        }
+        return sum;
+    }
+
+    public IntList getNumbersThatAddTo(int n, int target) {
+        boolean found = false;
+
+        MultiDimIndex i = new MultiDimIndex(n, this.size(), true);
+        IntList numbers = new IntList();
+
+        while(!found) {
+            if(this.sum(i) == target) {
+                for(int j = 0; j < i.length(); j++) {
+                    numbers.add(this.get(i.getIndex(j)));
+                    found = true;
+                }
+            }
+            i.iterateForward();
+        }
+        return numbers;
+    }
+
+    public long product() {
+        long product = 1;
+        for(Integer i : this) {
+            product *= i;
+        }
+        return product;
+    }
+}
--- src/code.java
+++ src/code.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -9,103 +8,28 @@ import java.util.ArrayList;
 
 import Helpers.*;
 
-import javax.swing.event.MouseInputListener;
-
 public class code {
+    final static Charset ENCODING = StandardCharsets.UTF_8;
+
     public static void main(String[] args) throws IOException {
         reportRepair();
     }
 
-    static void testMultiDimIndex() {
-        MultiDimIndex i = new MultiDimIndex(3, 3, false);
-        i.print();
-        i.iterateForward();
-        int j = 0;
-        while(j < 10) {
-            i.print();
-            i.iterateForward();
-            j++;
-        }
-    }
-
-
     static void reportRepair() throws IOException { //Day one
         String fileName = "src/Data/Day_1/input.txt";
         Path path = Paths.get(fileName);
-        Charset ENCODING = StandardCharsets.UTF_8;
-        ArrayList<Integer> numbers = convertArrayListOfStringtoInt((ArrayList<String>) Files.readAllLines(path, ENCODING));
-        //printArrayListInteger(numbers);
-        ArrayList<Integer> midSol1 = getNumbersThatAddTo(numbers, 2, 2020);
-        long solution1 = productOfIntegersInArrayList(midSol1);
-        ArrayList<Integer> midSol2 = getNumbersThatAddTo(numbers, 3, 2020);
-        long solution2 = productOfIntegersInArrayList(midSol2);
-        System.out.println(solution2);
-    }
 
+        IntList numbers = new IntList((ArrayList<String>) Files.readAllLines(path, ENCODING));
+        long solution1 = numbers.getNumbersThatAddTo(2, 2020).product();
+        long solution2 = numbers.getNumbersThatAddTo(1, 2020).product();
 
-    static ArrayList<Integer> convertArrayListOfStringtoInt(ArrayList<String> strings) {
-        ArrayList<Integer> integers = new ArrayList();
-        for (String s : strings) {
-            integers.add(Integer.parseInt(s));
-        }
-        return integers;
-    }
-
-    static ArrayList<Integer> getNumbersThatAddTo(ArrayList<Integer> list, int n, int target) {
-        boolean found = false;
-        final int MAXINDEX = list.size();
-
-        MultiDimIndex i = new MultiDimIndex(n, MAXINDEX, true);
-        ArrayList<Integer> numbers = new ArrayList();
-
-        while(!found) {
-            if(sumOfIntegersInListByIndex(list, i) == target) {
-                for(int j = 0; j < i.length(); j++) {
-                    numbers.add(list.get(i.getIndex(j)));
-                    found = true;
-                }
-            }
-            i.iterateForward();
-            i.print();
-        }
-
-        return numbers;
-    }
-
-    static int sumOfIntegersInListByIndex(ArrayList<Integer> list, MultiDimIndex is) {
-        int result = 0;
-        for(int i = 0; i < is.length(); i++) {
-            result += list.get(is.getIndex(i));
-        }
-        return result;
-    }
-
-    static long productOfIntegersInArrayList(ArrayList<Integer> list) {
-        int result = 1;
-        for (Integer n : list) {
-            result *= n;
-        }
-        return result;
-    }
-
-    static int sumOfIntegersInArrayList(ArrayList<Integer> list) {
-        int result = 0;
-        for (Integer n : list) {
-            result += n;
-        }
-        return result;
+        System.out.println("Solutions: " + solution1 + ", " + solution2);
     }
 
 
     static void printArrayListString(ArrayList<String> strings) {
         for (String s : strings) {
             System.out.println(s);
-        }
-    }
-
-    static void printArrayListInteger(ArrayList<Integer> integers) {
-        for (Integer i : integers) {
-            System.out.println(i.toString());
         }
     }
 }
