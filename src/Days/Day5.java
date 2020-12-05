21d1768ee66bc4a01ba7ff04cdd7a09b65c7be63
Peter Marsh <breadloaf64@gmail.com>
2020-12-05 23:11:13 +0000

    Completed day 5

---
+++ src/Days/Day5.java
@@ -0,0 +1,49 @@
+package Days;
+
+import Helpers.InputHandler;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Day5 {
+    public static void solve() throws IOException {
+        ArrayList<String> input = InputHandler.get("src/Data/Day_5/input.txt");
+        System.out.println(highestPass(input) + ", " + remainingPass(input));
+    }
+
+    private static int remainingPass(ArrayList<String> passes) {
+        ArrayList<Integer> allPasses = new ArrayList();
+        for (String s : passes) {
+            allPasses.add(seatID(s));
+        }
+        ArrayList<Integer> possiblePasses = new ArrayList();
+        for(int i = 0; i < 818; i++) {
+            if(allPasses.contains(i - 1) && allPasses.contains(i + 1)) {
+                possiblePasses.add(i);
+            }
+        }
+        for(Integer pass : allPasses) {
+            possiblePasses.remove(pass);
+        }
+        return possiblePasses.get(0);
+    }
+
+    private static int highestPass(ArrayList<String> passes) {
+        int highest = 0;
+        for(String s : passes) {
+            int ID = seatID(s);
+            if(ID > highest) highest = ID;
+        }
+        return highest;
+    }
+
+    private static int seatID(String seat) {
+        int row = 0, col = 0;
+        for(int i = 0; i < seat.length(); i++) {
+            if (seat.charAt(i) == 'B') row += Math.pow(2, 6 - i);
+            else if (seat.charAt(i) == 'R') col += Math.pow(2, 6 - i + 3);
+        }
+        return row * 8 + col;
+    }
+}
