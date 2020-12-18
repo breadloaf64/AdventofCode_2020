699238d2d009b52a90ccfba59fbcd2149b474049
Peter Marsh <breadloaf64@gmail.com>
2020-12-18 16:05:12 +0000

    Cleaned up day 17. Pretty slow, might optimise later

--- src/Days/Day17.java
+++ src/Days/Day17.java
@@ -9,40 +9,15 @@ public class Day17 {
 
     public static void solve() throws IOException {
         ArrayList<String> input = InputHandler.get("src/Data/Day_17/input.txt");
-        System.out.println(part1better(input));
-        //test(input);
+        System.out.println("Day 17: " + sixthIteration(input, 3) + ", " + sixthIteration(input, 4));
     }
 
-    private static int part1(ArrayList<String> input) {
-        ConwayCubes c = new ConwayCubes(input, 0);
-        c.draw();
+    private static int sixthIteration(ArrayList<String> input, int dimension) {
+        ConwayAutomaton c = new ConwayAutomaton(input, dimension, true);
         for(int i = 0 ; i < 6; i++) {
-            System.out.println("After cycle " + (i + 1) + ":");
             c = c.iterate();
-            c.draw();
-            System.out.println();
+            //System.out.println(i);
         }
         return c.count();
-    }
-
-    private static int part1better(ArrayList<String> input) {
-        ConwayAutomaton c = new ConwayAutomaton(input, 4, true);
-        //c.draw();
-        for(int i = 0 ; i < 6; i++) {
-            //System.out.println("After cycle " + (i + 1) + ":");
-            c = c.iterate();
-            //c.draw();
-            //System.out.println();
-        }
-        return c.count();
-    }
-
-
-    private static void test(ArrayList<String> input) {
-        MultiDimIndex i = new MultiDimIndex(3, 3);
-        while(!i.hasLooped()) {
-            i.print();
-            i.iterateForward();
-        }
     }
 }
